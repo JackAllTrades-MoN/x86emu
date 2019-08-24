85517316c65463a8ac0564a163fcc0a565e6dd4f
YukiSatake <satake@logic.cs.tsukuba.ac.jp>
2019-08-24 19:54:30 +0900

    fix utils for dump registers

--- rusty_emu/src/emulator/i386/core.rs
+++ rusty_emu/src/emulator/i386/core.rs
@@ -11,14 +11,49 @@ pub struct State {
     pub memory: Vec<u8>,
 }
 
+impl GRNames {
+    pub fn to_idx(&self) -> usize {
+        match self {
+            GRNames::EAX => 0, 
+            GRNames::EBX => 1,
+            GRNames::ECX => 2,
+            GRNames::EDX => 3,
+            GRNames::ESI => 4,
+            GRNames::EDI => 5,
+            GRNames::ESP => 6,
+            GRNames::EBP => 7,
+        }
+    }
+    pub fn to_string(&self) -> String {
+        match self {
+            GRNames::EAX => "EAX", 
+            GRNames::EBX => "EBX",
+            GRNames::ECX => "ECX",
+            GRNames::EDX => "EDX",
+            GRNames::ESI => "ESI",
+            GRNames::EDI => "EDI",
+            GRNames::ESP => "ESP",
+            GRNames::EBP => "EBP",
+        }.to_string()
+    }
+    pub fn all() -> [GRNames; 8]{
+        [GRNames::EAX, GRNames::EBX, GRNames::ECX,
+         GRNames::EDX, GRNames::ESI, GRNames::EDI,
+         GRNames::ESP, GRNames::EBP]
+    }
+}
+
 impl Register {
     pub fn init() -> Register {
         Register {general: [0x0000; 8], eflags: 0x0000, eip: 0x0000}
     }
     pub fn to_string(&self) -> String {
-        format!("EAX = 0x{}\nECX = 0x{}\n",
-                self.general[0],
-                self.general[1])
+        GRNames::all().iter().fold(
+            "".to_string(),
+            |acc, x| format!("{}\n{} = 0x{}",
+                             acc,
+                             x.to_string(),
+                             self.general[x.to_idx()]))
     }
 }
 
